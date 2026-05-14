b44d4f2c0b097e55ca7d02b9ff989fd0ff40c8f1
345954069 <345954069@C-43M5JB2.tdsb.on.ca>
2026-05-14 11:32:21 -0400

    pushing

--- src/Main.java
+++ src/Main.java
@@ -9,4 +9,10 @@
  */
 public class Main {
     
+    public static void main(String args[]) {
+        PrivacyCase privacyCase = new PrivacyCase("Data Leak Case", "Company exposed user emails publicly", "Personal Data");
+        privacyCase.verdict = new Verdict("Data Leak Case", "Unethical", "Personal Data"); 
+        
+        System.out.println(privacyCase);
+    }
 }
